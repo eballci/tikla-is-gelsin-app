fd405c8c4a1f017f3e9134c47eb081e72f0f3f06
Mehmet Keskin <keskinm90@gmail.com>
2023-05-15 22:58:05 +0300

    Add grid system and module css for greeting page style

--- src/pages/greeting/component.tsx
+++ src/pages/greeting/component.tsx
@@ -1,23 +1,67 @@
-import {IonButton, IonContent, IonPage,} from "@ionic/react";
-import {PageType, savePageType} from "./service";
-import {useHistory} from "react-router-dom";
-import useCheckPageType from "./use-check-page-type";
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonImg,
+  IonPage,
+  IonRow,
+  IonText,
+} from '@ionic/react';
+import { PageType, savePageType } from './service';
+import { useHistory } from 'react-router-dom';
+import InterviewImg from '../../assets/landing-img.jpg';
+import useCheckPageType from './use-check-page-type';
+import styles from './greeting.module.css';
 
 export default function Greeting() {
-    const history = useHistory();
-    const handlePageSelection = (pageType: PageType): void => {
-        savePageType(pageType);
-        history.push(`/${pageType.toString()}`);
-    };
+  const history = useHistory();
+  const handlePageSelection = (pageType: PageType): void => {
+    savePageType(pageType);
+    history.push(`/${pageType.toString()}`);
+  };
 
-    useCheckPageType();
+  useCheckPageType();
 
-    return (
-        <IonPage>
-            <IonContent>
-                <IonButton expand="block" onClick={() => handlePageSelection(PageType.SEEKER)}>İş Arayanlar</IonButton>
-                <IonButton expand="block" onClick={() => handlePageSelection(PageType.SEEKER)}>İş Verenler</IonButton>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+      <IonContent>
+        <div className={styles.flex}>
+          <IonGrid className={styles.center}>
+            <IonRow>
+              <IonCol>
+                <IonText>Tıkla: İş Gelsin'e Hoşgeldin</IonText>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <IonButton
+                  expand='block'
+                  onClick={() => handlePageSelection(PageType.SEEKER)}
+                >
+                  İş Arayanlar
+                </IonButton>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                {' '}
+                <IonButton
+                  expand='block'
+                  onClick={() => handlePageSelection(PageType.SEEKER)}
+                >
+                  İş Verenler
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+
+          <IonImg
+            src={InterviewImg}
+            alt='People waiting for job interview'
+          ></IonImg>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
 }
