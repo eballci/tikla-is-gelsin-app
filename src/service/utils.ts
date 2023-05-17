937d08e4aedfcefe799872126db579ed7a549ed4
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-18 00:18:01 +0300

    Make utils able to convert primitive level to status type

--- src/service/utils.ts
+++ src/service/utils.ts
@@ -1,4 +1,4 @@
-import {EducationLevel, LanguageLevel} from "../model";
+import {EducationLevel, LanguageLevel, OfferSubmissionStatus} from "../model";
 
 export const toLanguageLevel = (level: number): LanguageLevel => {
     if (level === 1) return LanguageLevel.BEGINNER;
@@ -12,4 +12,11 @@ export const toEducationLevel = (level: number): EducationLevel => {
     if (level === 2) return EducationLevel.BACHELORS;
     if (level === 3) return EducationLevel.MASTERS;
     return EducationLevel.DOCTORAL;
+}
+
+export const toOfferSuggestionStatus = (level: number): OfferSubmissionStatus => {
+    if (level === 1) return OfferSubmissionStatus.ISSUED;
+    if (level === 2) return OfferSubmissionStatus.IDLE;
+    if (level === 3) return OfferSubmissionStatus.ACCEPTED;
+    return OfferSubmissionStatus.REFUSED;
 }
