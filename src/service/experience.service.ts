82a22f3f1f073463aa84a0348e020d720e140012
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-19 00:04:52 +0300

    Complete experience removing service

--- src/service/experience.service.ts
+++ src/service/experience.service.ts
@@ -27,3 +27,15 @@ export const updateExperience = async (experience: Experience): Promise<boolean>
         return false;
     }
 };
+
+export const removeExperience = async (experience: Experience): Promise<boolean> => {
+    try {
+        const response = await axios.delete(
+            `http://localhost:8080/experience/${experience.id}`
+        );
+
+        return response.status === 200;
+    } catch (exception) {
+        return false;
+    }
+};
