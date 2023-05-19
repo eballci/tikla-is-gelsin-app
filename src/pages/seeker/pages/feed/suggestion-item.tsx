6f5442f1651cda2a010abbd96a59faca97e375c2
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-20 00:24:39 +0300

    Complete feed page by scaffolding way

---
+++ src/pages/seeker/pages/feed/suggestion-item.tsx
@@ -0,0 +1,161 @@
+import {
+    IonAvatar,
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonChip,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonModal,
+    IonText,
+    IonTitle,
+    IonToolbar,
+    useIonToast
+} from "@ionic/react";
+import React, {useState} from "react";
+import {PositionSuggestion} from "../../../../model";
+import {alertCircleOutline, alertOutline, checkmarkOutline, closeOutline, trashOutline} from "ionicons/icons";
+import {ignoreSuggestion, submitSuggestion} from "../../../../service/suggestion.service";
+import {useAppDispatch} from "../../../../store/hooks";
+import {fetchSeeker} from "../../../../store/store";
+
+export default function SuggestionItem({suggestion}: { suggestion: PositionSuggestion }) {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isIgnoring, setIsIgnoring] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isIgnored, setIsIgnored] = useState(false);
+    const [present] = useIonToast();
+    const dispatch = useAppDispatch();
+
+    const handleSubmit = () => {
+        setIsSubmitting(true);
+        submitSuggestion(suggestion).then((data) => {
+            if (!data) {
+                setTimeout(async () => {
+                    setIsSubmitting(false);
+                    await present({
+                        message: "Bağlantı sağlanamadı.",
+                        duration: 3000,
+                        position: "bottom",
+                        icon: alertOutline,
+                        color: "danger"
+                    });
+                }, 1500);
+                return;
+            }
+            setTimeout(async () => {
+                await present({
+                    message: "Başvuru yapıldı.",
+                    duration: 5000,
+                    position: "bottom",
+                    icon: checkmarkOutline,
+                    color: "success"
+                });
+                setIsSubmitted(true);
+            }, 1500);
+            setTimeout(() => dispatch(fetchSeeker()), 3500);
+        });
+    };
+
+    const handleIgnore = () => {
+        setIsIgnoring(true);
+        ignoreSuggestion(suggestion).then((data) => {
+            if (!data) {
+                setTimeout(async () => {
+                    setIsIgnoring(false);
+                    await present({
+                        message: "Bağlantı sağlanamadı.",
+                        duration: 3000,
+                        position: "bottom",
+                        icon: alertOutline,
+                        color: "danger"
+                    });
+                }, 1500);
+                return;
+            }
+            setTimeout(async () => {
+                await present({
+                    message: "Öneri kaldırıldı.",
+                    duration: 5000,
+                    position: "bottom",
+                    icon: trashOutline
+                });
+                setIsIgnored(true);
+            }, 1500);
+            setTimeout(() => dispatch(fetchSeeker()), 3500);
+        });
+    };
+
+    return (
+        <IonCard>
+            <IonCardHeader>
+                <IonAvatar>
+                    <img alt="Silhouette of a person's head"
+                         src="https://ionicframework.com/docs/img/demos/avatar.svg"/>
+                </IonAvatar>
+                <IonCardTitle>{suggestion.position.title}</IonCardTitle>
+                <IonCardSubtitle>%{suggestion.matchRate} Uyumluluk</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>{suggestion.position.description}</IonCardContent>
+            <IonButton onClick={() => {
+                setModalOpen(true)
+            }} expand="block">Detaylar</IonButton>
+            <IonModal isOpen={modalOpen}>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonButton onClick={() => {
+                                setModalOpen(false);
+                            }}>
+                                <IonIcon icon={closeOutline}></IonIcon>
+                            </IonButton>
+                        </IonButtons>
+                        <IonTitle>{suggestion.position.title}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <IonText>{suggestion.position.description}</IonText>
+                    <IonChip>%{suggestion.matchRate} Uyumluluk</IonChip>
+                    {
+                        (!isSubmitted && !isIgnored) && (
+                            <>
+                                <IonButton
+                                    disabled={isSubmitting || isIgnoring}
+                                    expand="block"
+                                    onClick={handleSubmit}>
+                                    {isSubmitting && "Başvuruluyor..."}
+                                    {!isSubmitting && "Başvur"}
+                                </IonButton>
+                                <IonButton
+                                    disabled={isSubmitting || isIgnoring}
+                                    expand="block"
+                                    fill="outline"
+                                    onClick={handleIgnore}>
+                                    {isIgnoring && "Kaldırılıyor..."}
+                                    {!isIgnoring && "Kaldır"}
+                                </IonButton>
+                            </>
+                        )
+                    }
+                    {
+                        isSubmitted && (
+                            <IonButton disabled fill="clear" expand="block">Başvuruldu</IonButton>
+                        )
+                    }
+                    {
+                        isIgnored && (
+                            <IonButton disabled fill="clear" expand="block">Kaldırıldı</IonButton>
+                        )
+                    }
+                </IonContent>
+            </IonModal>
+        </IonCard>
+    );
+}
--- src/pages/seeker/pages/feed/suggestion-list.tsx
+++ src/pages/seeker/pages/feed/suggestion-list.tsx
@@ -1,6 +1,7 @@
-import {IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from "@ionic/react";
+import {IonAvatar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from "@ionic/react";
 import {useAppSelector} from "../../../../store/hooks";
 import React from "react";
+import SuggestionItem from "./suggestion-item";
 
 export default function SuggestionList() {
     const suggestions = useAppSelector((state) => state.seeker.me?.suggestions);
@@ -8,14 +9,7 @@ export default function SuggestionList() {
         <>
             {
                 suggestions?.map(suggestion => (
-                    <IonCard key={suggestion.id}>
-                        <IonCardHeader>
-                            <IonCardTitle>{suggestion.position.title}</IonCardTitle>
-                            <IonCardSubtitle>%{suggestion.matchRate} Uyumluluk</IonCardSubtitle>
-                        </IonCardHeader>
-
-                        <IonCardContent>{suggestion.position.description}</IonCardContent>
-                    </IonCard>
+                    <SuggestionItem key={suggestion.id} suggestion={suggestion}></SuggestionItem>
                 ))
             }
         </>
