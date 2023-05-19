d1ef59063f97ec574af3f313b71dca691dd0c9e0
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-19 11:22:34 +0300

    Complete employer getting service

--- src/service/employer.service.ts
+++ src/service/employer.service.ts
@@ -1,5 +1,8 @@
-import {VisitEmployer} from "../model";
+import {Employer, Position, VisitEmployer, VisitSeeker} from "../model";
 import axios from "axios";
+import {getPosition} from "./position.service";
+import {toOfferSuggestionStatus} from "./utils";
+import {visitSeeker} from "./seeker.service";
 
 export const visitEmployer = async (id: number): Promise<VisitEmployer> => {
     try {
@@ -18,4 +21,71 @@ export const visitEmployer = async (id: number): Promise<VisitEmployer> => {
     } catch (exception) {
         throw new Error("The requested employer couldn't found");
     }
-}
+};
+
+export const getEmployer = async (id: number): Promise<Employer> => {
+    try {
+        const response = await axios.get(`http://localhost:8080/employer/me/${id}`);
+        const data = response.data;
+        const requestedPositions: Array<Position> = [];
+        const requestedSeekers: Array<VisitSeeker> = [];
+        const requestedPositionIds = new Set([
+            ...data.openPositions.map((position: any) => position.id)
+        ]);
+        const requestedSeekerIds = new Set([
+            ...data.submissions.map((submission: any) => submission.seekerId),
+            ...data.offers.map((offer: any) => offer.seekerId),
+            ...data.suggestions.map((suggestion: any) => suggestion.seekerId),
+        ]);
+
+        for (let id of requestedPositionIds) {
+            requestedPositions.push(await getPosition(id));
+        }
+
+        for (let id of requestedSeekerIds) {
+            requestedSeekers.push(await visitSeeker(id));
+        }
+
+        return {
+            id: data.id,
+            avatar: data.avatar,
+            name: data.name,
+            description: data.description,
+            email: data.email,
+            webSite: data.webSite,
+            scale: data.scale,
+            openPositions: requestedPositions,
+            submissions: [
+                ...data.submissions.map((submission: any) => ({
+                    id: submission.id,
+                    seeker: requestedSeekers.filter(s => s.id === submission.seekerId)[0],
+                    position: requestedPositions.filter(p => p.id === submission.positionId)[0],
+                    status: toOfferSuggestionStatus(submission.status),
+                    createdAt: new Date(submission.createdAt)
+                }))
+            ],
+            offers: [
+                ...data.offers.map((offer: any) => ({
+                    id: offer.id,
+                    seeker: requestedSeekers.filter(s => s.id === offer.seekerId)[0],
+                    position: requestedPositions.filter(p => p.id === offer.positionId)[0],
+                    status: toOfferSuggestionStatus(offer.status),
+                    createdAt: new Date(offer.createdAt)
+                }))
+            ],
+            //çok fazla suggestion kaydı geliyor.
+            suggestions: [
+                ...data.suggestions.map((suggestion: any) => ({
+                    id: suggestion.id,
+                    seeker: requestedSeekers.filter(s => s.id === suggestion.seekerId)[0],
+                    position: requestedPositions.filter(p => p.id === suggestion.positionId)[0],
+                    matchRate: suggestion.matchRate,
+                    createdAt: new Date(suggestion.createdAt)
+                }))
+            ]
+
+        };
+    } catch (exception) {
+        throw new Error("The requested employer couldn't found");
+    }
+};
