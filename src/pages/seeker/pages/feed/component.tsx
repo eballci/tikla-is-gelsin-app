fb786412837de1c10f4e63ea89cd6a832ef826bf
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-19 20:47:50 +0300

    Fulfill feed component for several states

--- src/pages/seeker/pages/feed/component.tsx
+++ src/pages/seeker/pages/feed/component.tsx
@@ -11,10 +11,15 @@ import {
 import {useAppDispatch, useAppSelector} from "../../../../store/hooks";
 import {fetchSeeker} from "../../../../store/store";
 import Skeleton from "./skeleton";
+import SuggestionList from "./suggestion-list";
+import NoData from "./no-data";
+import Error from "./error";
 
 export default function Feed() {
     const dispatch = useAppDispatch();
     const isFetching = useAppSelector((state) => state.seeker.isFetching);
+    const isFetchingFailed = useAppSelector((state) => state.seeker.isFetchingFailed);
+    const suggestionCount = useAppSelector((state) => state.seeker.me?.suggestions)?.length;
 
     const handleRefresh = (event: CustomEvent<RefresherEventDetail>) => {
         dispatch(fetchSeeker(() => {
@@ -38,7 +43,10 @@ export default function Feed() {
                 <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
                     <IonRefresherContent></IonRefresherContent>
                 </IonRefresher>
-                {isFetching && <Skeleton/>}
+                {(isFetching && !isFetchingFailed) && <Skeleton/>}
+                {(!isFetching && !isFetchingFailed) && <SuggestionList/>}
+                {(suggestionCount === 0 && !isFetchingFailed) && <NoData/>}
+                {isFetchingFailed && <Error/>}
             </IonContent>
         </IonPage>
     );
