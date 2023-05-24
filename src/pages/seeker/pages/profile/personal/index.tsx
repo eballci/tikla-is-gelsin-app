712b28fc6cbc6f8fb1255f9ce6ffcea42a9cda07
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-24 14:47:19 +0300

    Complete seeker avatar phase

--- src/pages/seeker/pages/profile/personal/index.tsx
+++ src/pages/seeker/pages/profile/personal/index.tsx
@@ -1,17 +1,92 @@
-import {IonAvatar, IonIcon, IonItem, IonItemGroup, IonLabel, IonText, IonTitle} from "@ionic/react";
-import {createOutline} from "ionicons/icons";
+import {
+    IonAvatar,
+    IonIcon,
+    IonItem,
+    IonItemGroup,
+    IonLabel,
+    IonText,
+    IonTitle,
+    useIonActionSheet,
+    useIonToast
+} from "@ionic/react";
+import {alertOutline, createOutline, trashOutline} from "ionicons/icons";
 import React from "react";
-import {useAppSelector} from "../../../../../store/hooks";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks";
+import {Camera, CameraResultType} from "@capacitor/camera";
+import {updateAvatar} from "../../../../../service/seeker.service";
+import {fetchSeeker} from "../../../../../store/store";
 
 export default function Personal() {
+    const dispatch = useAppDispatch();
+    const [present] = useIonActionSheet();
+    const [presentToast] = useIonToast();
     const seeker = useAppSelector((state) => state.seeker.me);
+
+    const handleAvatarClick = async () => {
+        await present({
+            header: "Fotoğraf",
+            buttons: [
+                {
+                    text: 'Fotoğrafı Güncelle',
+                    handler: handleUpdateAvatar
+                },
+                {
+                    text: 'İptal',
+                    role: 'cancel',
+                    data: {
+                        action: 'cancel',
+                    },
+                }
+            ]
+        })
+    };
+
+    const handleUpdateAvatar = async () => {
+        const image = await Camera.getPhoto({
+            quality: 90,
+            resultType: CameraResultType.Uri
+        });
+        const response = await fetch(image.webPath ?? "");
+        const blob = await response.blob();
+        const file = new File([blob], 'photo.jpg', {
+            type: 'image/jpeg',
+        });
+
+        const data = await updateAvatar(seeker?.id ?? 0, file);
+
+        if (!data) {
+            await presentToast({
+                message: "Profil fotoğrafı güncellenemedi.",
+                duration: 3000,
+                position: "bottom",
+                icon: alertOutline,
+                color: "danger"
+            });
+            return;
+        }
+        await presentToast({
+            message: "Profil fotoğrafı güncellendi.",
+            duration: 5000,
+            position: "bottom",
+            icon: trashOutline,
+            color: "success"
+        });
+        setTimeout(() => dispatch(fetchSeeker()), 3500);
+    };
 
     return (
         <IonItemGroup>
             <IonItem lines="none">
-                <IonAvatar style={{width: 100, height: 100}}>
+                <IonAvatar
+                    onClick={handleAvatarClick}
+                    style={{width: 100, height: 100}}>
                     <img alt="Profile image of the seeker"
-                         src={seeker?.avatar === "" ? "https://ionicframework.com/docs/img/demos/avatar.svg" : seeker?.avatar}/>
+                         src={seeker?.avatar === ""
+                             ?
+                             "https://ionicframework.com/docs/img/demos/avatar.svg"
+                             :
+                             "data:image/png;base64," + seeker?.avatar
+                         }/>
                 </IonAvatar>
             </IonItem>
             <IonItem lines="none" className="ion-padding-top">
