44868e7431327b04482fdb98124572800aad4512
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-27 10:24:20 +0300

    Complete error and no-data components for offers page

--- src/pages/seeker/pages/offers/no-data.tsx
+++ src/pages/seeker/pages/offers/no-data.tsx
@@ -1,7 +1,28 @@
-import {IonText} from "@ionic/react";
+import {IonChip, IonCol, IonContent, IonGrid, IonIcon, IonRow, IonText} from "@ionic/react";
+import {informationOutline} from "ionicons/icons";
 
 export default function NoData() {
     return (
-        <IonText>No data</IonText>
+        <IonContent className="ion-padding">
+            <IonChip style={{height: 140}}
+                     outline={true}
+                     color="secondary">
+                <IonGrid>
+                    <IonRow class="ion-align-items-center">
+                        <IonCol size="auto">
+                            <IonIcon icon={informationOutline}></IonIcon>
+                        </IonCol>
+                        <IonCol className="ion-padding-start ion-padding-end"
+                                style={{textAlign: "justify"}}>
+                            <IonText>
+                                İş ilanı teklifiniz bulunmamaktadır. Teklif almanın yolu
+                                profil/ilan eşleşmesinden geçmektedir. Bu sebeple profilinizi
+                                zenginleştirmenizi öneiriz.
+                            </IonText>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonChip>
+        </IonContent>
     )
 }
