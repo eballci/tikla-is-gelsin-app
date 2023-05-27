0e23331f9ea64b5d826408f3791e7e4401d73ef6
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-27 10:28:32 +0300

    Complete error component for profile page

--- src/pages/seeker/pages/profile/error.tsx
+++ src/pages/seeker/pages/profile/error.tsx
@@ -1,7 +1,26 @@
-import {IonText} from "@ionic/react";
+import {IonChip, IonCol, IonContent, IonGrid, IonIcon, IonRow, IonText} from "@ionic/react";
+import {alertOutline} from "ionicons/icons";
 
 export default function Error() {
     return (
-        <IonText color="danger">Network Error</IonText>
+        <IonContent className="ion-padding">
+            <IonChip style={{height: 80}}
+                     outline={true}
+                     color="danger">
+                <IonGrid>
+                    <IonRow class="ion-align-items-center">
+                        <IonCol size="auto">
+                            <IonIcon icon={alertOutline}></IonIcon>
+                        </IonCol>
+                        <IonCol className="ion-padding-start ion-padding-end"
+                                style={{textAlign: "justify"}}>
+                            <IonText>
+                                Bağlantıda problem meydana geldi. Lütfen daha sonra tekrar deneyiniz.
+                            </IonText>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonChip>
+        </IonContent>
     );
 }
