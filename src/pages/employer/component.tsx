8fe70bd788980122da588896aa79212488f6c8c7
Emre BALCI <emre-balci@outlook.com.tr>
2023-05-30 11:11:42 +0300

    Complete employer app routing and authentication

--- src/pages/employer/component.tsx
+++ src/pages/employer/component.tsx
@@ -1,45 +1,97 @@
-import {
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
-    IonCardSubtitle,
-    IonCardTitle,
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-} from "@ionic/react";
-import React from "react";
+import {IonBadge, IonIcon, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs, TabsCustomEvent} from "@ionic/react";
+import React, {useEffect} from "react";
 import {PageType, savePageType} from "../../root/persistent";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {fetchEmployer, resetSubmissionNews} from "../../store/store";
+import {readAllSubmissions} from "../../service/submission.service";
+import {IonReactRouter} from "@ionic/react-router";
+import {albumsOutline, briefcaseOutline, businessOutline, paperPlaneOutline, peopleCircleOutline} from "ionicons/icons";
+import {Route} from "react-router-dom";
+import {Redirect} from "react-router";
+import Feed from "./pages/feed";
+import Offers from "./pages/offers";
+import Submissions from "./pages/submissions";
+import Profile from "./pages/profile";
+import Login from "./pages/login";
+import Positions from "./pages/positions";
 
 export default function Employer() {
+    const submissionNews = useAppSelector((state) => state.employer.submissionNews);
+    const employerId = useAppSelector((state) => state.employer.id);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (employerId !== 0)
+            dispatch(fetchEmployer());
+    }, [employerId]);
+
+    const handleWhenTabChangedToSubmissions = (event: TabsCustomEvent): void => {
+        if (event.detail.tab === "submissions") {
+            setTimeout(async () => {
+                dispatch(resetSubmissionNews());
+                await readAllSubmissions(employerId);
+            }, 250);
+        }
+    }
 
     savePageType(PageType.EMPLOYER);
 
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Tab 1</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Tab 1</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <IonCard>
-                    <IonCardHeader>
-                        <IonCardTitle>Card Title</IonCardTitle>
-                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-                    </IonCardHeader>
-
-                    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing
-                        less.</IonCardContent>
-                </IonCard>
-            </IonContent>
-        </IonPage>
+        <IonReactRouter>
+            <IonTabs onIonTabsDidChange={handleWhenTabChangedToSubmissions}>
+                <IonRouterOutlet>
+                    <Route exact path="/employer">
+                        {
+                            employerId !== 0 ? (
+                                <Redirect to="/employer/feed"/>
+                            ) : (
+                                <Redirect to="/employer/login"/>
+                            )
+                        }
+                    </Route>
+                    {
+                        employerId !== 0 ? (
+                            <>
+                                <Route path="/employer/feed" exact component={Feed}/>
+                                <Route path="/employer/positions" exact component={Positions}/>
+                                <Route path="/employer/offers" exact component={Offers}/>
+                                <Route path="/employer/submissions" exact component={Submissions}/>
+                                <Route path="/employer/profile" exact component={Profile}/>
+                                <Route path="/employer/">
+                                    <Redirect to="/employer/feed"/>
+                                </Route>
+                            </>
+                        ) : (
+                            <>
+                                <Route path="/employer/login" exact render={() => <Login/>}/>
+                                <Route path="/employer/">
+                                    <Redirect to="/employer/login"/>
+                                </Route>
+                            </>
+                        )
+                    }
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                    <IonTabButton tab="feed" href="/employer/feed">
+                        <IonIcon aria-hidden="true" icon={albumsOutline}/>
+                    </IonTabButton>
+                    <IonTabButton tab="positions" href="/employer/positions">
+                        <IonIcon aria-hidden="true" icon={briefcaseOutline}/>
+                    </IonTabButton>
+                    <IonTabButton tab="offers" href="/employer/offers">
+                        <IonIcon aria-hidden="true" icon={paperPlaneOutline}/>
+                    </IonTabButton>
+                    <IonTabButton tab="submissions" href="/employer/submissions">
+                        {
+                            submissionNews > 0 ? (<IonBadge>{submissionNews}</IonBadge>) : (<></>)
+                        }
+                        <IonIcon aria-hidden="true" icon={peopleCircleOutline}/>
+                    </IonTabButton>
+                    <IonTabButton tab="profile" href="/employer/profile">
+                        <IonIcon aria-hidden="true" icon={businessOutline}/>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
     );
 }
--- src/pages/employer/pages/login/component.tsx
+++ src/pages/employer/pages/login/component.tsx
@@ -1,5 +1,122 @@
+import React, {useState} from "react";
+import {
+    IonButton,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonInput,
+    IonModal,
+    IonRow,
+    IonSpinner,
+    IonTitle,
+    useIonToast
+} from "@ionic/react";
+import {Redirect} from "react-router";
+import {useAppDispatch, useAppSelector} from "../../../../store/hooks";
+import {alertOutline, checkmarkOutline} from "ionicons/icons";
+import {setEmployerId} from "../../../../store/store";
+import {loginEmployer} from "../../../../service/employer.service";
+
 export default function Login() {
+    const dispatch = useAppDispatch();
+    const employerId = useAppSelector((state) => state.employer.id);
+    const [present] = useIonToast();
+    const [isLogging, setIsLogging] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        setIsLogging(true);
+        loginEmployer(email, password).then((data) => {
+            if (data === 0) {
+                setTimeout(async () => {
+                    setIsLogging(false);
+                    await present({
+                        message: "E-Posta ya da parola yanlış girildi",
+                        duration: 3000,
+                        position: "bottom",
+                        icon: alertOutline,
+                        color: "danger"
+                    });
+                }, 1500);
+                return;
+            }
+            setTimeout(async () => {
+                await present({
+                    message: "Giriş yapıldı.",
+                    duration: 5000,
+                    position: "bottom",
+                    icon: checkmarkOutline,
+                    color: "success"
+                });
+                dispatch(setEmployerId(data));
+            }, 1500);
+        });
+    };
+
     return (
-        <div>login</div>
+        <>
+            <IonModal trigger="open-modal" isOpen={true}>
+                <IonContent className="ion-padding">
+                    {
+                        (employerId !== 0) && <Redirect to="/employer/feed"/>
+                    }
+                    <IonGrid className="ion-padding-top">
+                        <IonRow style={{paddingTop: 150}}>
+                            <IonCol>
+                                <IonTitle size="large">
+                                    Giriş yapın
+                                </IonTitle>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol className="ion-padding">
+                                <IonInput
+                                    disabled={isLogging}
+                                    className="ion-text-left"
+                                    type="email"
+                                    fill="solid"
+                                    label="e-posta"
+                                    labelPlacement="floating"
+                                    helperText="örnek: misal@alan.com"
+                                    onIonInput={(ev) => setEmail(ev.detail.value as string)}
+                                />
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol className="ion-padding">
+                                <IonInput
+                                    disabled={isLogging}
+                                    className="ion-text-left"
+                                    type="password"
+                                    fill="solid"
+                                    label="parola"
+                                    labelPlacement="floating"
+                                    helperText="parolanızı girin"
+                                    onIonInput={(ev) => setPassword(ev.detail.value as string)}
+                                />
+                            </IonCol>
+                        </IonRow>
+                        <IonRow className="ion-align-items-center">
+                            <IonCol>
+                                <IonButton
+                                    style={{width: 100}}
+                                    onClick={handleLogin}
+                                    disabled={isLogging}
+                                    fill="outline">
+                                    {
+                                        (isLogging) ? (
+                                            <IonSpinner/>
+                                        ) : (
+                                            "Giriş Yap"
+                                        )
+                                    }
+                                </IonButton>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonContent>
+            </IonModal>
+        </>
     );
 }
