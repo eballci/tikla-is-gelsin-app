3fbc7e606f71b39fef66a02ec91eb7773a981fe5
Emre BALCI <emre-balci@outlook.com.tr>
2023-06-04 15:27:04 +0300

    Implement employer suggestion buttons functionality

--- src/pages/employer/pages/feed/candidate-item.tsx
+++ src/pages/employer/pages/feed/candidate-item.tsx
@@ -2,23 +2,97 @@ import {SeekerSuggestion} from "../../../../model";
 import {
     IonAvatar,
     IonButton,
-    IonButtons, IonCol,
+    IonButtons,
+    IonCol,
     IonContent,
-    IonFooter, IonGrid,
+    IonFooter,
+    IonGrid,
     IonHeader,
     IonIcon,
     IonItem,
     IonLabel,
-    IonModal, IonRow, IonTitle,
-    IonToolbar
+    IonModal,
+    IonRow,
+    IonSpinner,
+    IonTitle,
+    IonToolbar,
+    useIonToast
 } from "@ionic/react";
 import React, {useState} from "react";
-import {closeOutline} from "ionicons/icons";
+import {alertOutline, checkmarkOutline, closeOutline, trashOutline} from "ionicons/icons";
 import styles from "./feed.module.css";
 import Candidate from "./candidate";
+import {useAppDispatch} from "../../../../store/hooks";
+import {ignoreSuggestion, offerSuggestion} from "../../../../service/suggestion.service";
+import {fetchEmployer} from "../../../../store/store";
 
 export default function CandidateItem({suggestion}: { suggestion: SeekerSuggestion }) {
     const [modalOpen, setModalOpen] = useState(false);
+    const [isOffering, setIsOffering] = useState(false);
+    const [isIgnoring, setIsIgnoring] = useState(false);
+    const [isOffered, setIsOffered] = useState(false);
+    const [isIgnored, setIsIgnored] = useState(false);
+    const [present] = useIonToast();
+    const dispatch = useAppDispatch();
+
+    const handleOffer = async () => {
+        setIsOffering(true);
+        const data = await offerSuggestion(suggestion);
+        if (!data) {
+            setTimeout(async () => {
+                setIsOffering(false);
+                await present({
+                    message: 'Bağlantı sağlanamadı.',
+                    duration: 3000,
+                    position: 'bottom',
+                    icon: alertOutline,
+                    color: 'danger',
+                });
+            }, 1500);
+            return;
+        }
+        setTimeout(async () => {
+            await present({
+                message: 'Teklif sunuldu.',
+                duration: 5000,
+                position: 'bottom',
+                icon: checkmarkOutline,
+                color: 'success',
+            });
+            setIsOffered(true);
+        }, 1500);
+        setTimeout(() => dispatch(fetchEmployer()), 3500);
+    };
+
+    const handleIgnore = () => {
+        setIsIgnoring(true);
+        ignoreSuggestion(suggestion).then((data) => {
+            if (!data) {
+                setTimeout(async () => {
+                    setIsIgnoring(false);
+                    await present({
+                        message: 'Bağlantı sağlanamadı.',
+                        duration: 3000,
+                        position: 'bottom',
+                        icon: alertOutline,
+                        color: 'danger',
+                    });
+                }, 1500);
+                return;
+            }
+            setTimeout(async () => {
+                await present({
+                    message: 'Aday önerisi kaldırıldı.',
+                    duration: 5000,
+                    position: 'bottom',
+                    icon: trashOutline,
+                });
+                setIsIgnored(true);
+            }, 1500);
+            setTimeout(() => dispatch(fetchEmployer()), 3500);
+        });
+    };
+
 
     return (
         <>
@@ -62,20 +136,62 @@ export default function CandidateItem({suggestion}: { suggestion: SeekerSuggesti
                         <IonRow className={[styles['compatibility-high'], styles.badge].join(' ')}>
                             %{suggestion.matchRate} Uyumluluk
                         </IonRow>
-                        <IonRow>
-                            <IonCol>
-                                <IonButton shape="round"
-                                           expand="block"
-                                           style={{}}
-                                           fill="outline">Yok say</IonButton>
-                            </IonCol>
-                            <IonCol>
-                                <IonButton shape="round"
-                                           expand="block"
-                                           style={{}}
-                                           fill="solid">Teklif gönder</IonButton>
-                            </IonCol>
-                        </IonRow>
+                        {
+                            (!isOffered && !isIgnored) && (
+                                <IonRow>
+                                    <IonCol>
+                                        <IonButton shape="round"
+                                                   disabled={isOffering || isIgnoring}
+                                                   expand="block"
+                                                   onClick={handleIgnore}
+                                                   fill="outline">
+                                            {
+                                                isIgnoring ? <IonSpinner/> : "Yok say"
+                                            }
+                                        </IonButton>
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton shape="round"
+                                                   disabled={isOffering || isIgnoring}
+                                                   expand="block"
+                                                   onClick={handleOffer}
+                                                   fill="solid">
+                                            {
+                                                isOffering ? <IonSpinner/> : "Teklif gönder"
+                                            }
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                            )
+                        }
+                        {
+                            isOffered && (
+                                <IonRow>
+                                    <IonCol>
+                                        <IonButton shape="round"
+                                                   expand="block"
+                                                   fill="clear"
+                                                   disabled={true}>
+                                            Teklif sunuldu.
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                            )
+                        }
+                        {
+                            isIgnored && (
+                                <IonRow>
+                                    <IonCol>
+                                        <IonButton shape="round"
+                                                   expand="block"
+                                                   fill="clear"
+                                                   disabled={true}>
+                                            Öneri kaldırıldı.
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                            )
+                        }
                     </IonGrid>
                 </IonFooter>
             </IonModal>
