ef3b3a116ba9053688826d93628af8e946e74631
Emre BALCI <emre-balci@outlook.com.tr>
2023-06-05 20:45:43 +0300

    Implement position detail functionality

--- src/pages/employer/pages/positions/position-detail.tsx
+++ src/pages/employer/pages/positions/position-detail.tsx
@@ -1,7 +1,148 @@
-import {IonButton, IonButtons, IonHeader, IonIcon, IonModal, IonTitle, IonToolbar} from "@ionic/react";
+import {
+    IonButton,
+    IonButtons,
+    IonChip,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonItemDivider,
+    IonItemGroup,
+    IonList,
+    IonModal,
+    IonRow,
+    IonText,
+    IonTitle,
+    IonToolbar
+} from "@ionic/react";
 import {closeOutline} from "ionicons/icons";
-import {Position} from "../../../../model";
-import {Positions} from "../../../../data/presetData";
+import {EducationCriteria, ExperienceCriteria, LanguageCriteria, Position} from "../../../../model";
+import {
+    EducationLevelTranslation,
+    LanguageLevelTranslation,
+    Languages,
+    Positions,
+    Studies
+} from "../../../../data/presetData";
+import PositionInModal from "../../../../components/position-in-modal";
+import React from "react";
+
+function LanguageCriteriaList(
+    {criteriaList}: { criteriaList: Array<LanguageCriteria> }
+) {
+    return (
+        <IonItemGroup>
+            <IonItemDivider>Dil Kriterleri</IonItemDivider>
+            {
+                criteriaList.map(criteria => (
+                    <IonItem>
+                        <IonText>
+                            {
+                                Languages
+                                    .filter(l => l.value === criteria.expectedLanguage)
+                                    [0]
+                                    .visual
+                            }
+                            {" "}
+                            <IonChip>
+                                {
+                                    LanguageLevelTranslation
+                                        .filter(level => level.value === criteria.expectedLevel)
+                                        [0]
+                                        .visual
+                                }
+                            </IonChip>
+                        </IonText>
+                    </IonItem>
+                ))
+            }
+            {
+                criteriaList.length === 0 && (
+                    <IonItem>
+                        <IonText>Bu ilan dil kriteri içermiyor.</IonText>
+                    </IonItem>
+                )
+            }
+        </IonItemGroup>
+    );
+}
+
+function EducationCriteriaList(
+    {criteriaList}: { criteriaList: Array<EducationCriteria> }
+) {
+    return (
+        <IonItemGroup>
+            <IonItemDivider>Eğitim Kriterleri</IonItemDivider>
+            {
+                criteriaList.map(criteria => (
+                    <IonItem>
+                        <IonText>
+                            {
+                                Studies
+                                    .filter(l => l.value === criteria.study)
+                                    [0]
+                                    .visual
+                            }
+                            {" "}
+                            <IonChip>
+                                {
+                                    EducationLevelTranslation
+                                        .filter(level => level.value === criteria.minEducationLevel)
+                                        [0]
+                                        .visual
+                                }
+                            </IonChip>
+                        </IonText>
+                    </IonItem>
+                ))
+            }
+            {
+                criteriaList.length === 0 && (
+                    <IonItem>
+                        <IonText>Bu ilan eğitim kriteri içermiyor.</IonText>
+                    </IonItem>
+                )
+            }
+        </IonItemGroup>
+    );
+}
+
+function ExperienceCriteriaList(
+    {criteriaList}: { criteriaList: Array<ExperienceCriteria> }
+) {
+    return (
+        <IonItemGroup>
+            <IonItemDivider>Tecrübe Kriterleri</IonItemDivider>
+            {
+                criteriaList.map(criteria => (
+                    <IonItem>
+                        <IonText>
+                            {
+                                Positions
+                                    .filter(l => l.value === criteria.title)
+                                    [0]
+                                    .visual
+                            }
+                            {" "}
+                            <IonChip>
+                                +{criteria.minimumYears} yıl
+                            </IonChip>
+                        </IonText>
+                    </IonItem>
+                ))
+            }
+            {
+                criteriaList.length === 0 && (
+                    <IonItem>
+                        <IonText>Bu ilan tecrübe kriteri içermiyor.</IonText>
+                    </IonItem>
+                )
+            }
+        </IonItemGroup>
+    );
+}
 
 export default function PositionDetail(
     {
@@ -35,6 +176,21 @@ export default function PositionDetail(
                     </IonTitle>
                 </IonToolbar>
             </IonHeader>
+            <IonContent>
+                <PositionInModal position={position}/>
+                <IonGrid className="ion-padding">
+                    <IonRow>
+                        <IonCol>
+                            <h2 className="ion-text-left">Kriterler</h2>
+                        </IonCol>
+                    </IonRow>
+                    <IonList>
+                        <LanguageCriteriaList criteriaList={position.languageCriteriaList}/>
+                        <EducationCriteriaList criteriaList={position.educationCriteriaList}/>
+                        <ExperienceCriteriaList criteriaList={position.experienceCriteriaList}/>
+                    </IonList>
+                </IonGrid>
+            </IonContent>
         </IonModal>
     )
 }
