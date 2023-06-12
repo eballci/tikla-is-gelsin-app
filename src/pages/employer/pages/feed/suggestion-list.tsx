3a520c47cd2886fdd9e9c52c147121084678b585
Emre BALCI <emre-balci@outlook.com.tr>
2023-06-12 19:25:54 +0300

    Avoid recreating of position cards

--- src/pages/employer/pages/feed/suggestion-list.tsx
+++ src/pages/employer/pages/feed/suggestion-list.tsx
@@ -4,6 +4,7 @@ import SuggestionItem from "./suggestion-item";
 export default function SuggestionList() {
     const suggestions = useAppSelector((state) => state.employer.me?.suggestions);
     const suggestedPositions = (suggestions?.map(suggestion => suggestion.position) ?? [])
+        .sort((a, b) => (a.id - b.id))
         .filter((position, index, array) => (
             !index || position.id !== array[index - 1].id
         ));
